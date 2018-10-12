ff44b5b787fc4a440c83a0b06f62ed016286a7f0
SapphireExile <15312520+SapphireExile@users.noreply.github.com>
2018-10-12 17:57:11 -0400

    Update main.cpp
    Removed commandline args
    Modified mode selection
    Fixed seg faults

--- main.cpp
+++ main.cpp
@@ -11,10 +11,13 @@
 #include <sys/sysinfo.h>
 #include <stdlib.h>
 
+
 int KernelDirectorySpookiness = 0;
 int NumberOfThreads = 0;
 int CheckInput = -99;
 int SkipMode = 0;
+// const char *Mode = "Nothing";
+const char *CMLArguments = NULL;
 const char *BasicMake = "sudo make -j";
 const char *BasicMakeInstall = "sudo make install -j";
 const char *BasicMakeModulesInstall = "sudo make modules_install -j";
@@ -28,60 +31,17 @@ const char *HighPerfMakeInstall = "sudo make install -j";
 const char *HighPerfMakeModulesInstall = "sudo make modules_install -j";
 const char *HighPerfMakeModules = "sudo make modules -j";
 
-int main(int argc, char *argv[])
+
+int main()
 {
-	// Hidden command to stop segfault
-	// ! Doesn't appear to be working.
-	*argv = "nothing";
-	// Ton of helpful shit here. Command line arguments.
-	if (strcmp(argv[1], "-basic") == 0 || strcmp(argv[1], "-Basic") == 0)
-	{
-		SkipMode = 1;
-		std::string Mode = "1";
-		CheckInput = 1;
-	}
-	if (strcmp(argv[1], "-LowPerf") == 0 || strcmp(argv[1], "-lowperf") == 0)
-	{
-		SkipMode = 2;
-		std::string Mode = "2";
-		CheckInput = 2;
-	}
-	if (strcmp(argv[1], "-HighPerf") == 0 || strcmp(argv[1], "-highperf") == 0)
-	{
-		SkipMode = 3;
-		std::string Mode = "3";
-		CheckInput = 3;
-	}
-	if (strcmp(argv[1], "-Expert") == 0 || strcmp(argv[1], "-expert") == 0)
-	{
-		SkipMode = 4;
-		std::string Mode = "4";
-		CheckInput = 4;
-	}
-	if (strcmp(argv[1], "-IgnoreDir") == 0 || strcmp(argv[1], "-ignoredir") == 0)
-	{
-		KernelDirectorySpookiness = 99;
-	}
-	if (strcmp(argv[1], "-id") == 0 || strcmp(argv[1], "-ID") == 0)
-	{
-		KernelDirectorySpookiness = 99;
-	}
-	if (strcmp(argv[1], "-help") == 0 || strcmp(argv[1], "-Help") == 0 || strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "-H") == 0)
-	{
-		printf("Commands: \n");
-		printf("-IgnoreDir / -ignoredir / -id / -ID: Disable directory checking. Useful when I can't detect if your directory contains a kernel.\n");
-		printf("-help / -Help / -h / -H: Shows this help dialogue.\n");
-		printf("-basic / -lowperf / -highperf / -expert: Sets the mode, and skips mode checking.");
-		printf("Press enter to exit.");
-		std::cin.ignore();
-		return 0;
-	}
+	// Debug flags
+	KernelDirectorySpookiness = 99;
 
 	// Main annoucement
-    printf("Sapphire's Kernel Installer!\n");
+	printf("Sapphire's Kernel Installer!\n");
 	printf("Version 1.0\n");
 
-// Check if this is a kernel directory
+	// Check if this is a kernel directory
 	if (access("vmlinux-gdb.py", F_OK) != -1) {
 		KernelDirectorySpookiness = KernelDirectorySpookiness + 1;
 	}
@@ -105,50 +65,52 @@ int main(int argc, char *argv[])
 	}
 
 	// Select kernel mode
-	if (SkipMode = 0) {
-		std::string Mode;
-		printf("Select mode:\n");
-		printf("1. Basic\n");
-		printf("2. Low performance\n");
-		printf("3. High performance\n");
-		printf("4. Expert\n");
-		std::getline(std::cin, Mode);
-		NumberOfThreads = sysconf(_SC_NPROCESSORS_ONLN);
-		std::system("clear");
-		// Workaround for mode selection failure
-		// ! Does not appear to be working
-		if (Mode == "4") {
-			CheckInput = 4;
-			if (Mode == "3") {
-				CheckInput = 3;
-				if (Mode == "2") {
-					CheckInput = 2;
-					if (Mode == "1") {
-						CheckInput = 1;
-					}
+	printf("Select mode:\n");
+	printf("1. Basic\n");
+	printf("2. Low performance\n");
+	printf("3. High performance\n");
+	printf("4. Expert\n");
+	char Mode[50];
+	fgets(Mode, sizeof Mode, stdin);
+	NumberOfThreads = sysconf(_SC_NPROCESSORS_ONLN);
+	std::system("clear");
+	// Workaround for mode selection failure
+	// ! Does not appear to be working
+	if (strcmp(Mode, "4")) {
+		(CheckInput = 4);
+		printf("Expert\n");
+	}
+	else {
+		if (strcmp(Mode, "3")) {
+			CheckInput = 3;
+			printf("High perf\n");
+		}
+		else {
+			if (strcmp(Mode, "2")) {
+				CheckInput = 2;
+				printf("Low perf\n");
+			}
+			else {
+				if (strcmp(Mode, "1")) {
+					CheckInput = 1;
+					printf("Basic\n");
+				}
+				else {
+					system("clear");
+					printf("Invalid selection. Only use 1, 2, 3, or 4.\n");
+					printf("Press enter to exit.\n");
+					std::cin.ignore();
+					return 0;
 				}
 			}
 		}
 	}
 
-	if (CheckInput < 1) {
-		system("clear");
-		printf("Invalid selection. Only use 1, 2, 3, or 4.");
-		printf("Press enter to exit.");
-		std::cin.ignore();
-		return 0;
-	}
-	if (CheckInput > 4) {
-		system("clear");
-		printf("Invalid selection. Only use 1, 2, 3, or 4.");
-		printf("Press enter to exit.");
-		std::cin.ignore();
-		return 0;
-	}
+	// Debug
+	printf("Selection allowed.\n");
+	return 0;
 
-
-
-// Check if the main config file is there
+	// Check if the main config file is there
 	if (access(".config", F_OK) != -1) {
 		printf("Config found.\n");
 	}
@@ -176,7 +138,7 @@ int main(int argc, char *argv[])
 		printf("Cleaning the output.");
 		system("make clean");
 		// Make this function better. Array? Foreach? 
-		if (NumberOfThreads == 1 || NumberOfThreads ==  3 || NumberOfThreads ==  5 || NumberOfThreads ==  NumberOfThreads ==  7 || NumberOfThreads ==  9 || NumberOfThreads ==  11 || NumberOfThreads ==  13 || NumberOfThreads ==  15 || NumberOfThreads ==  17 || NumberOfThreads ==  19 || NumberOfThreads ==  21 || NumberOfThreads ==  23 || NumberOfThreads ==  25 || NumberOfThreads ==  27 || NumberOfThreads ==  29 || NumberOfThreads ==  31 || NumberOfThreads ==  33 || NumberOfThreads ==  35 || NumberOfThreads ==  37 || NumberOfThreads ==  39 || NumberOfThreads ==  41 || NumberOfThreads ==  43 || NumberOfThreads ==  45 || NumberOfThreads ==  47 || NumberOfThreads ==  49 || NumberOfThreads ==  51 || NumberOfThreads ==  53 || NumberOfThreads ==  55 || NumberOfThreads ==  57 || NumberOfThreads ==  59 || NumberOfThreads ==  61 || NumberOfThreads ==  63 || NumberOfThreads ==  65 || NumberOfThreads == 67 || NumberOfThreads ==  69) {
+		if (NumberOfThreads == 1 || NumberOfThreads == 3 || NumberOfThreads == 5 || NumberOfThreads == NumberOfThreads == 7 || NumberOfThreads == 9 || NumberOfThreads == 11 || NumberOfThreads == 13 || NumberOfThreads == 15 || NumberOfThreads == 17 || NumberOfThreads == 19 || NumberOfThreads == 21 || NumberOfThreads == 23 || NumberOfThreads == 25 || NumberOfThreads == 27 || NumberOfThreads == 29 || NumberOfThreads == 31 || NumberOfThreads == 33 || NumberOfThreads == 35 || NumberOfThreads == 37 || NumberOfThreads == 39 || NumberOfThreads == 41 || NumberOfThreads == 43 || NumberOfThreads == 45 || NumberOfThreads == 47 || NumberOfThreads == 49 || NumberOfThreads == 51 || NumberOfThreads == 53 || NumberOfThreads == 55 || NumberOfThreads == 57 || NumberOfThreads == 59 || NumberOfThreads == 61 || NumberOfThreads == 63 || NumberOfThreads == 65 || NumberOfThreads == 67 || NumberOfThreads == 69) {
 			NumberOfThreads = NumberOfThreads + 1;
 		}
 		NumberOfThreads = NumberOfThreads / 2;
@@ -224,20 +186,22 @@ int main(int argc, char *argv[])
 	// This section needs a lot of work. All kernel parameters must be exposed and usable.
 	if (CheckInput = 4) {
 		printf("Expert mode is disabled in this release.");
-	// printf("Cleaning the output.");
-	// system("make clean");
-	// (BasicMakeModulesInstall += NumberOfThreads);
-	// system("clear");
-	// (BasicMakeInstall += NumberOfThreads);
-	// system("clear");
-	// printf("Making the modules for installation.");
-	// system(BasicMakeModulesInstall);
-	// printf("Making and installing the kernel.");
-	// system(BasicMakeInstall);
-	// printf("All done. Reboot and enjoy.");
+		// printf("Cleaning the output.");
+		// system("make clean");
+		// (BasicMakeModulesInstall += NumberOfThreads);
+		// system("clear");
+		// (BasicMakeInstall += NumberOfThreads);
+		// system("clear");
+		// printf("Making the modules for installation.");
+		// system(BasicMakeModulesInstall);
+		// printf("Making and installing the kernel.");
+		// system(BasicMakeInstall);
+		// printf("All done. Reboot and enjoy.");
 	}
-//End of file. If everything goes well, return 0.
+	//End of file. If everything goes well, return 0.
 	printf("Press enter to exit.");
 	std::cin.ignore();
-    return 0;
+	return 0;
 }
+
+
