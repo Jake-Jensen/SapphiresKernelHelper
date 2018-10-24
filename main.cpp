727486472587461ac2df6ee93c2a259edb70d548
Jake Jensen <15312520+SapphireExile@users.noreply.github.com>
2018-10-24 18:28:10 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,13 +18,13 @@
 
 int KernelDirectorySpookiness = 0;
 int SkipCheck = 0;
+char _Name[50];
 
 
 void Check()
 {
 	if (access(".config", F_OK) != -1) {
 		printf("Config found.\n");
-
 	}
 	else {
 		printf("Config not found. Generating.\n");
@@ -249,6 +249,47 @@ void Expert()
 	exit(1);
 }
 
+// void DistName()
+// {
+// 	printf("Specify a name for this kernel.\n");
+// 	printf("Name should be all lowercase and not contain symbols.\n");
+// 	char _Name[50];
+// 	fgets(_Name, sizeof _Name, stdin);
+// 	_Name[strcspn(_Name, "\n")] = 0;
+// 
+// 
+// }
+
+void Distribution()
+{
+	Check();
+
+	system("make clean");
+	int ret = system("make clean");
+	if (WEXITSTATUS(ret) == 0x10) {
+		int ret = system("fakeroot make-kpkg --initrd --append-to-version=-custom kernel_image kernel_headers modules -j `getconf _NPROCESSORS_ONLN`");
+		if (WEXITSTATUS(ret) == 0x10) {
+			printf("All done. Reboot and enjoy\n");
+			exit(0);
+		}
+		else {
+			printf("Failed to compile kernel for distribution.\n");
+			printf("Press enter to exit.");
+			std::cin.ignore();
+			exit(1);
+		}
+	}
+	else {
+		printf("Failed to clean the output.\n");
+		printf("Press enter to exit.");
+		std::cin.ignore();
+		exit(1);
+	}
+
+	printf("All done. Reboot and enjoy.\n");
+	exit(0);
+}
+
 
 int main()
 {
@@ -258,7 +299,7 @@ int main()
 
 	// Main annoucement
 	printf("Sapphire's Kernel Installer!\n");
-	printf("Version 1.0\n");
+	printf("Version 1.1\n");
 
 	// Check if this is a kernel directory
 	if (access("vmlinux-gdb.py", F_OK) != -1) {
@@ -289,9 +330,21 @@ int main()
 	printf("2. Low performance\n");
 	printf("3. High performance\n");
 	printf("4. Expert\n");
+	printf("5. Distribution\n");
 	char Mode[50];
 	fgets(Mode, sizeof Mode, stdin);
 	std::system("clear");
+	if (strcmp(Mode, "5\n") == 0) {
+		system("clear");
+		printf("Gathering dependencies.\n");
+		sleep(1);
+		GetDepends();
+		// Disabled in current release
+		// DistName();
+		printf("Starting build.\n");
+		sleep(1);
+		Distribution();
+	}
 	if (strcmp(Mode, "4\n") == 0) {
 		system("clear");
 		printf("Gathering dependencies.\n");
@@ -331,7 +384,7 @@ int main()
 	}
 
 	system("clear");
-	printf("Invalid selection. Only use 1, 2, 3, or 4.\n");
+	printf("Invalid selection. Only use 1, 2, 3, 4, or 5.\n");
 	printf("Press enter to exit.\n");
 	std::cin.ignore();
 	return 0;
@@ -342,5 +395,3 @@ int main()
 	std::cin.ignore();
 	return 0;
 }
-
-
